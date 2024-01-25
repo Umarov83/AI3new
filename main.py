e71197c4e27f48ebca67a5a7692d4af6fa870b28
Umarov83 <ya.ooo.83@yandex.ru>
2024-01-25 17:54:38 +0300

    ver.2

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import asyncio
 import os
 
-import requests
-from bs4 import BeautifulSoup
-
 from aiogram import Bot, Dispatcher, types
-from aiogram.filters import CommandStart, Command
+
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
 
 from handlers.user_private import user_private_router
+from handlers.user_group import user_group_router
+from common.bot_cmds_list import private
 
 ALLOWED_UPDATES = ['message, edited_message']
 
@@ -18,28 +17,14 @@ bot = Bot(token=os.getenv('TOKEN'))
 dp = Dispatcher()
 
 dp.include_router(user_private_router)
+dp.include_router(user_group_router)
 
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
+    # await bot.delete_my_commands(scope=types.BotCommandScopeAllPrivateChats())
+    await bot.set_my_commands(commands=private, scope=types.BotCommandScopeAllPrivateChats())
     await dp.start_polling(bot, allowed_updates=ALLOWED_UPDATES)
-
-
-url = 'https://yandex.ru/video/search?text=porno-hub'
-
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-link_all = []
-
-for video_soup in soup.find_all('div', class_='VideoSnippet2-Content'):
-    link = video_soup.div.a['href']
-    link_all = link
-
-
-@user_private_router.message(Command('Video'))
-async def menu_cmd(message: types.Message):
-    await message.answer(link_all)
 
 
 asyncio.run(main())
